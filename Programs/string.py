222f920d245a491954abefd9731b0e7d8c010cf7
Chethan P <132081469+chethanp1@users.noreply.github.com>
2023-07-21 23:01:33 +0530

    Add files via upload

---
+++ Programs/string.py
@@ -0,0 +1,12 @@
+# String concatination using (+) adddition operator.
+str1 = 'RCB '
+str2 = 'Won the match'
+a = str1 + str2
+print("string After concatination: ")
+print(a)
+#String concatination using format.
+var1 = "Bharat"
+var2 = "petrol"
+print("{} {}".format(var1, var2))
+var3 = "{} {}".format(str1, str2)
+print(var3)
