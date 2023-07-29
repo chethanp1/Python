279b9fd6d28a2dc1f2bb7d8656db365e763a3d3f
Chethan P <132081469+chethanp1@users.noreply.github.com>
2023-07-29 23:24:17 +0530

    Add files via upload

---
+++ Programs/convert.py
@@ -0,0 +1,8 @@
+# Read in upper case and print in lower case.
+str = input("Enter the string in upper case. ")
+# covert to lowercase using lower().
+a=str.lower()
+print("String after convert to lower : ")
+print(a)
+# covert to lowercase using swapcase().
+print(str.swapcase())
