ccc4027d98a89da9bd2b7a97e25752ab7f3106a9
Chethan P <132081469+chethanp1@users.noreply.github.com>
2024-12-08 23:51:23 +0530

    Add files via upload

---
+++ Exercise/Find Smallest & Largest Numbers in a List.py
@@ -0,0 +1,10 @@
+# Approach 1 using sort() method
+mylist=[76,23,55,88]
+mylist.sort()
+print("Smallest element is :",mylist[0])
+print("Largest element is :", mylist[-1])
+
+# Approach 2 using max and min function
+mylist=[76,23,55,88]
+print("Smallest element is :",min(mylist))
+print("Largest element is :", max(mylist))
