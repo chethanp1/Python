3bd93212ddf5a7563f46837b0e69af4633aa8580
Chethan P <132081469+chethanp1@users.noreply.github.com>
2024-12-10 23:54:48 +0530

    Add files via upload

---
+++ Exercise/finding the length.py
@@ -0,0 +1,12 @@
+# Approach 1
+mylist=[1,2,3,4,5,6,7]
+print(mylist)
+count=0
+for i in mylist:
+    count=count+1
+print("Length of list is :", count)
+
+# Approach 2
+mylist=[1,2,3,4,5,6]
+print(mylist)
+print("Lenght of the list is :",len(mylist))
