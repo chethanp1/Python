bb1ee9b01b3f48a5f8a8debbf36e6f6d3d2c1c65
Chethan P <132081469+chethanp1@users.noreply.github.com>
2024-12-29 23:28:45 +0530

    Add files via upload

---
+++ Pattern based question.py
@@ -0,0 +1,12 @@
+n=5
+for i in range(n,0,-1):
+    for j in range(i):
+        print(j, end=" ")
+    print()
+
+
+n=5
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print(j, end=" ")
+    print()
